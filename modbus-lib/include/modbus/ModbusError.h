b97d5945e20309f5512fe00c7f62d188838ac08e
salaheldeenyasser <salaheldeenyasser@gmail.com>
2026-03-11 01:32:52 +0200

    created ModbusError.h

--- modbus-lib/include/modbus/ModbusError.h
+++ modbus-lib/include/modbus/ModbusError.h
@@ -1,6 +1,8 @@
 #ifndef MODBUS_ERROR_H
 #define MODBUS_ERROR_H
 #include <cstdint>
+#include <string>
+#include <variant>
 
 enum class TransportErrorCode{
     Timeout,
@@ -9,7 +11,21 @@ enum class TransportErrorCode{
     WriteError,
 };
 
+enum class ProtocolErrorCode{
+    CrcMismatch,
+    InvalidFrameLength,
+    SlaveIdMismatch,
+    UnexpectedResponse,
+};
 
+struct ModbusException{
+    uint8_t functionCode;
+    uint8_t exceptionCode;
+    std::string description() const;
+};
+
+using ModbusError = std::variant<TransportErrorCode, ProtocolErrorCode, ModbusException>;
+    
 
 
 #endif // MODBUS_ERROR_H
