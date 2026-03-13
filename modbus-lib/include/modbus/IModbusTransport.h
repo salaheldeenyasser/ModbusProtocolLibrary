e0810309009be0d07d7173fd93a2d3d5e7983768
salaheldeenyasser <salaheldeenyasser@gmail.com>
2026-03-13 15:27:49 +0200

    refactor: Reorder includes and improve formatting in IModbusTransport header

--- modbus-lib/include/modbus/IModbusTransport.h
+++ modbus-lib/include/modbus/IModbusTransport.h
@@ -1,14 +1,20 @@
 #ifndef MODBUS_ITRANSPORT_H
 #define MODBUS_ITRANSPORT_H
-#include "ModbusTypes.h"
 #include "ModbusError.h"
+#include "ModbusTypes.h"
 
-class IModbusTransport{
+class IModbusTransport
+{
 public:
     virtual ~IModbusTransport() = default;
     virtual std::expected<size_t, ModbusError> send(
         std::span<const u8> data) = 0;
 
-    virtual std::expected<std::vector<u8>, ModbusError> receive(size_t expectedLength, std::chrono::milliseconds timeout) = 0;
+    virtual std::expected<std::vector<u8>, ModbusError> receive(
+        size_t expectedLength, std::chrono::milliseconds timeout) = 0;
+
+    virtual void flush() = 0;
+    virtual bool isOpen() const = 0;
+    virtual void close() = 0;
 };
 #endif // MODBUS_ITRANSPORT_H
